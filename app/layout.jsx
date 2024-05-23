16aaae1486a9f8a906ac5d5891915617955ef2cd
PavansaiKaviti <pavan94901@gmail.com>
2024-05-23 18:08:35 -0500

    navbar in layout to place static

---
+++ app/layout.jsx
@@ -0,0 +1,15 @@
+import "@/assets/styles/global.css";
+import Navbar from "@/components/navbar/Navbar";
+
+const layout = ({ children }) => {
+  return (
+    <html>
+      <body>
+        <Navbar />
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+};
+
+export default layout;
