089d7b57a4905625368131a8b854998a11068c79
PavansaiKaviti <pavan94901@gmail.com>
2024-05-24 14:52:49 -0500

    added spinner

---
+++ app/loading.jsx
@@ -0,0 +1,19 @@
+"use client";
+import ClipLoader from "react-spinners/ClipLoader";
+const override = {
+  display: "block",
+  margin: "100px auto",
+};
+const Loadingpage = ({ loading }) => {
+  return (
+    <ClipLoader
+      color="black"
+      loading={loading}
+      cssOverride={override}
+      size={150}
+      aria-label="Loading Spinner"
+    />
+  );
+};
+
+export default Loadingpage;
