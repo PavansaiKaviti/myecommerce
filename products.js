d742897feef50ecfb29431e32560a8f253cad58e
PavansaiKaviti <pavan94901@gmail.com>
2024-05-24 00:45:07 -0500

    added json product dummy data

---
+++ products.js
@@ -0,0 +1,56 @@
+const products = [
+  {
+    _id: "1",
+    name: "Airpods Max",
+    image: "/images/airpodmax.jpeg",
+    description:
+      "Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working",
+    brand: "Apple",
+    category: "Electronics",
+    price: 299.99,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    _id: "2",
+    name: "macbook air",
+    image: "/images/mac.jpeg",
+    description:
+      "Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working",
+    brand: "Apple",
+    category: "Electronics",
+    price: 1399.99,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    _id: "3",
+    name: "iphone 15 max",
+    image: "/images/iphone.jpeg",
+    description:
+      "Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working",
+    brand: "Apple",
+    category: "Electronics",
+    price: 599.99,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    _id: "4",
+    name: "airpods 2G",
+    image: "/images/airpods.jpg",
+    description:
+      "Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working",
+    brand: "Apple",
+    category: "Electronics",
+    price: 399.99,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+];
+
+export default products;
