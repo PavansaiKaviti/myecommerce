a775e83e8b746cd57b312fce332b8acc8e415421
PavansaiKaviti <pavan94901@gmail.com>
2024-05-24 14:51:49 -0500

    added products from api

--- app/products/page.jsx
+++ app/products/page.jsx
@@ -1,11 +1,19 @@
+import Productcard from "@/components/productcard/Productcard";
+import { fetchProducts } from "@/utils/requests/Requests";
+
 import React from "react";
 
 export const metadata = {
   title: "products",
 };
 
-const Productpage = () => {
-  return <div>products</div>;
+const Productpage = async () => {
+  const products = await fetchProducts();
+  return (
+    <div>
+      <Productcard products={products} />
+    </div>
+  );
 };
 
 export default Productpage;
