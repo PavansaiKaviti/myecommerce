1d681f24887ddc85efbdb2c34a3b05b42744bff6
PavansaiKaviti <pavan94901@gmail.com>
2024-05-27 21:41:37 -0500

    modified button color

--- components/productcard/Productcard.jsx
+++ components/productcard/Productcard.jsx
@@ -4,11 +4,11 @@ import Link from "next/link";
 
 const Productcard = ({ products }) => {
   return (
-    <section class="px-4 py-6">
-      <div class="container-xl lg:container m-auto px-4 py-6">
-        <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+    <section className="px-4 py-6">
+      <div className="container-xl lg:container m-auto px-4 py-6">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
           {products.map((product) => (
-            <div class="rounded-xl shadow-lg relative" key={product._id}>
+            <div className="rounded-xl shadow-lg relative" key={product._id}>
               <Image
                 src={product.image}
                 alt="airpods"
@@ -16,19 +16,20 @@ const Productcard = ({ products }) => {
                 width={0}
                 height={0}
                 sizes="100"
+                priority={true}
               />
-              <div class="p-4">
-                <div class="text-center mb-6">
-                  <h3 class="text-xl font-bold">{product.name}</h3>
+              <div className="p-4">
+                <div className="text-center mb-6">
+                  <h3 className="text-xl font-bold">{product.name}</h3>
                 </div>
-                <div class="flex justify-center gap-4 text-gray-500 mb-4">
+                <div className="flex justify-center gap-4 text-gray-500 mb-4">
                   <p className="text-xl font-bold">${product.price}</p>
                 </div>
-                <div class="border border-gray-100 mb-5"></div>
-                <div class="flex flex-col  justify-center lg:flex-row  mb-4">
+                <div className="border border-gray-100 mb-5"></div>
+                <div className="flex flex-col  justify-center lg:flex-row  mb-4">
                   <Link
-                    href="property.html"
-                    class="h-[36px] bg-gray-500 hover:bg-black text-white px-4 py-2 rounded-lg text-center text-sm"
+                    href={`/products/${product._id}`}
+                    className="h-[36px] bg-gray-500 hover:bg-black text-white px-4 py-2 rounded-lg text-center text-sm"
                   >
                     BUY
                   </Link>
