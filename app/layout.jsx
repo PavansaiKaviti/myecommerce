4f575a795e7a9626ac46f2270bd8aff0fef44da7
PavansaiKaviti <pavan94901@gmail.com>
2024-05-27 21:36:10 -0500

    added cart count

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,14 +1,24 @@
 import "@/assets/styles/global.css";
+import Googleprovider from "@/components/googleprovider/Googleprovider";
 import Navbar from "@/components/navbar/Navbar";
+import Reduxprovider from "./globalstore/Provider";
+
+export const metadata = {
+  title: "ecommerce",
+};
 
 const layout = ({ children }) => {
   return (
-    <html>
-      <body>
-        <Navbar />
-        <main>{children}</main>
-      </body>
-    </html>
+    <Googleprovider>
+      <html lang="en">
+        <Reduxprovider>
+          <body>
+            <Navbar />
+            <main>{children}</main>
+          </body>
+        </Reduxprovider>
+      </html>
+    </Googleprovider>
   );
 };
 
