b65643287967a37b382a2b13c9613d916ffd080f
PavansaiKaviti <pavan94901@gmail.com>
2024-07-03 00:20:27 -0500

    mongoodb, cloudinary configuration

--- config/mongoose/Mongoosedb.jsx
+++ config/mongoose/Mongoosedb.jsx
@@ -1,13 +1,19 @@
 import mongoose from "mongoose";
 import colors from "colors";
 import Product from "@/models/products/Productmodel";
+import User from "@/models/usermodel/Usermodel";
+import Bgimage from "@/models/bgmodel/Backgroundimage";
+
 let connected = false;
 
 //models
 Product();
+User();
+Bgimage();
 
 const connectdb = async () => {
   // Enable strict query mode throws if there is any undefined fields are used by Mongoodb,
+
   mongoose.set("strictQuery", true);
   // if its already connected
   if (connected) {
