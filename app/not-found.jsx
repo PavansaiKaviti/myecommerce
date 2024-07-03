74ec2ba091f294534d9b60fa09371acaf755431a
PavansaiKaviti <pavan94901@gmail.com>
2024-07-03 00:06:46 -0500

    clientside ui for 404

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -6,7 +6,7 @@ import pagelost from "@/public/images/pagenotfound.jpg";
 
 const Nopage = () => {
   return (
-    <div className="flex flex-col gap-5 justify-center items-center mt-10">
+    <div className="flex flex-col gap-5 justify-center items-center mt-10 mb-6">
       <div>
         <Image
           src={pagelost}
@@ -17,9 +17,9 @@ const Nopage = () => {
         />
       </div>
       <div className="text-3xl">Oops page not found....</div>
-      <div className="border rounded-xl bg-black hover:bg-blue-500 ">
-        <Link href="/" className="p-3 text-white">
-          <FaArrowLeft className="inline m-1" />
+      <div className="border flex justify-center rounded-xl bg-black hover:bg-blue-500 ">
+        <Link href="/" className="p-2  text-white">
+          <FaArrowLeft className="inline mr-1" />
           Go Back
         </Link>
       </div>
