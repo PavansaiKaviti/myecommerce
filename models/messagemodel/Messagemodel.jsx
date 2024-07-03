f8fbc2a6d272df311abf419d697c6ba48f45d0e7
PavansaiKaviti <pavan94901@gmail.com>
2024-07-03 00:21:55 -0500

    model for message

---
+++ models/messagemodel/Messagemodel.jsx
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    product: {
+      type: mongoose.Types.ObjectId,
+      ref: "Product",
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Message =
+  mongoose.models.Message || mongoose.model("Message", messageSchema);
+
+export default Message;
