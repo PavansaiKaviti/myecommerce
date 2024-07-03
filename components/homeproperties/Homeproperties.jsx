fad9bf0ed53b6c7b6bcdc60b71d5627d1f16dc0c
PavansaiKaviti <pavan94901@gmail.com>
2024-07-03 00:14:26 -0500

    home products updated with fetch

--- components/homeproperties/Homeproperties.jsx
+++ components/homeproperties/Homeproperties.jsx
@@ -1,21 +1,45 @@
 "use client";
 import Productcard from "@/components/productcard/Productcard";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
+
+// export async function getStaticProps() {
+//   try {
+//     const res = await fetch(`${process.env.NEXT_PUBLIC_DOMAIN_API}/products`);
+//     if (!res.ok) {
+//       throw new Error("Failed to fetch products");
+//     }
+//     const data = await res.json();
+//     return {
+//       props: {
+//         products: data.products,
+//       },
+//       revalidate: 60, // Revalidate every 60 seconds
+//     };
+//   } catch (error) {
+//     console.error(error);
+//     return {
+//       props: {
+//         products: [],
+//       },
+//     };
+//   }
+// }
 
 const Homeproperties = () => {
+  const [isLoaded, setIsLoaded] = useState(false);
   const [products, setProducts] = useState([]);
   useEffect(() => {
     const fetchallproducts = async () => {
       try {
         const res = await fetch(
-          `${process.env.NEXT_PUBLIC_DOMAIN_API}/products`,
-          { cache: "no-store" }
+          `${process.env.NEXT_PUBLIC_DOMAIN_API}/products`
         );
         if (!res.ok) {
           return;
         }
         const data = await res.json();
-        setProducts(data);
+        setProducts(data.products);
+        setIsLoaded(true);
       } catch (error) {
         console.log(error);
       }
@@ -23,12 +47,18 @@ const Homeproperties = () => {
     if (products.length === 0) {
       fetchallproducts();
     }
-  }, []);
-  const newProducts = products.sort((a, b) => Math.random() - 0.5).slice(0, 3);
+  }, [products]);
+  // const newProducts = products.sort((a, b) => Math.random() - 0.5).slice(0, 3);
+  const shuffledProducts = useMemo(() => {
+    if (isLoaded) {
+      return products.sort(() => Math.random() - 0.5).slice(0, 3);
+    }
+    return [];
+  }, [products, isLoaded]);
 
   return (
     <div>
-      <Productcard products={newProducts} />
+      <Productcard products={shuffledProducts} />
     </div>
   );
 };
