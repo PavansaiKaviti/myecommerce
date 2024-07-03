ed64a2cea273ecbed4bb96fe63ca01807f8c6e60
PavansaiKaviti <pavan94901@gmail.com>
2024-07-02 23:56:16 -0500

    added page to store

--- app/globalstore/Store.jsx
+++ app/globalstore/Store.jsx
@@ -1,11 +1,12 @@
 "use client";
-
 import { configureStore } from "@reduxjs/toolkit";
 import cartsliceReducer from "@/app/globalstore/reduxslices/cartslice/Cart";
+import pageReducer from "@/app/globalstore/reduxslices/pageSlice/Pageslice";
 
 const Reduxstore = configureStore({
   reducer: {
     cart: cartsliceReducer,
+    page: pageReducer,
   },
 });
 
