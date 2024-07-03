d8536cba9e5b95a2d21bfb5ffe53ee2166f90d80
PavansaiKaviti <pavan94901@gmail.com>
2024-07-03 00:09:06 -0500

    clientside ui for first page for home products and carousel

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,10 @@
+import Carousel from "@/components/carousel/Carousel";
 import Homeproperties from "@/components/homeproperties/Homeproperties";
 
 const page = () => {
   return (
     <div>
+      <Carousel />
       <Homeproperties />
     </div>
   );
